e487ef17d151e80aac1b77f70818a476ccc715e2
Phillip Johnson <pdj1183@gmail.com>
2023-09-12 16:44:32 -0600

    adc terminal

--- nrf9160-ADC-Terminal/src/main.c
+++ nrf9160-ADC-Terminal/src/main.c
@@ -28,14 +28,24 @@ static const struct adc_channel_cfg m_1st_channel_cfg = {
 	.reference = ADC_REFERENCE,
 	.acquisition_time = ADC_ACQUISITION_TIME,
 	.channel_id = ADC_1ST_CHANNEL_ID,
-    .differential = 1,
+    .differential = 0,
 #if defined(CONFIG_ADC_CONFIGURABLE_INPUTS)
 	.input_positive = ADC_1ST_CHANNEL_INPUT,
-    .input_negative = ADC_2ND_CHANNEL_INPUT,
 #endif
 };
 
-#define BUFFER_SIZE 1
+static const struct adc_channel_cfg m_2nd_channel_cfg = {
+	.gain = ADC_GAIN,
+	.reference = ADC_REFERENCE,
+	.acquisition_time = ADC_ACQUISITION_TIME,
+	.channel_id = ADC_2ND_CHANNEL_ID,
+    .differential = 0,
+#if defined(CONFIG_ADC_CONFIGURABLE_INPUTS)
+	.input_positive = ADC_2ND_CHANNEL_INPUT,
+#endif
+};
+
+#define BUFFER_SIZE 2
 static int16_t m_sample_buffer[BUFFER_SIZE];
 
 
@@ -44,7 +54,7 @@ static int adc_sample(int volt)
 	int ret;
 
 	const struct adc_sequence sequence = {
-		.channels = BIT(ADC_1ST_CHANNEL_ID),
+		.channels = BIT(ADC_1ST_CHANNEL_ID) | BIT(ADC_2ND_CHANNEL_ID),
 		.buffer = m_sample_buffer,
 		.buffer_size = sizeof(m_sample_buffer),
 		.resolution = ADC_RESOLUTION,
@@ -57,58 +67,27 @@ static int adc_sample(int volt)
 	ret = adc_read(adc_dev, &sequence);
 
 	/* Print the AIN0 values */
+	for (int i = 0; i < BUFFER_SIZE; i++){
+		int pin = 14 + i;
+		int32_t adc_voltage = m_sample_buffer[i];
+		printk("ADC raw value p%d: %d\n", pin, m_sample_buffer[i]);
 
-	int32_t adc_voltage = m_sample_buffer[0];
-	printk("ADC raw value: %d\n", m_sample_buffer[0]);
+		ret = adc_raw_to_millivolts(adc_ref_internal(adc_dev), ADC_GAIN, ADC_RESOLUTION, &adc_voltage);
+		if (ret)
+		{
+			printk("raw_to_mili Broke!");
+			return ret;
+		}
+		
+		volt = (int) adc_voltage;
+		printk("Measured voltage p%d: %d mV\n", pin, volt);
 
-	ret = adc_raw_to_millivolts(adc_ref_internal(adc_dev), ADC_GAIN, ADC_RESOLUTION-1, &adc_voltage);
-	if (ret)
-	{
-		printk("raw_to_mili Broke!");
-		return ret;
 	}
-	
-	volt = (int) adc_voltage;
-	printk("Measured voltage: %d mV\n", volt);
 
 	return ret;
 }
-/*
-int get_adc_reading(float *reading) {
-
-	struct gpio_dt_spec p17 = GPIO_DT_SPEC_GET(DT_NODELABEL(led0), gpios);
-	if (!device_is_ready(p17.port)) {
-		return -1;
-	}
-	gpio_pin_configure_dt(&p17, GPIO_OUTPUT);
-
-	gpio_pin_set_dt(&p17, 1);
-
-	int err;
-	float volt = 0;
-
-	err = adc_channel_setup(adc_dev, &m_1st_channel_cfg);
-	if (err) {
-		printk("Error in adc setup: %d\n", err);
-		return err;
-	}
 
 
-	NRF_SAADC_NS->TASKS_CALIBRATEOFFSET = 1;
-	for (int i = 0; i <= 20; i++) {
-		err = adc_sample(volt);
-		if (err) {
-			printk("Error in adc sampling: %d\n", err);
-			return err;
-		}
-		k_msleep(SLEEP_TIME_MS);
-	}
-	*reading = volt;
-
-	gpio_pin_set_dt(&p17, 0);
-	return 0;
-}
-*/
 int main(void)
 {
 	struct gpio_dt_spec p17 = GPIO_DT_SPEC_GET(DT_NODELABEL(led0), gpios);
@@ -139,11 +118,6 @@ int main(void)
 		printk("Error in adc setup: %d\n", err);
 		return err;
 	}
-
-	printk("nrf91 saadc sampling AIN0 (P0.13)\n");
-	printk("Example requires secure_boot to have ");
-	printk("SAADC set to non-secure!\n");
-	printk("If not; BusFault/UsageFault will be triggered\n");
 
 	/* Trigger offset calibration
 	 * As this generates a _DONE and _RESULT event
