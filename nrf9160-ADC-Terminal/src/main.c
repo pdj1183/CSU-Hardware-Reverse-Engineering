143709744ff9fb364282ee372df1c771b4b0fab5
Phillip Johnson <pdj1183@gmail.com>
2023-10-30 16:02:50 -0600

    New config ADC terminal

--- nrf9160-ADC-Terminal/src/main.c
+++ nrf9160-ADC-Terminal/src/main.c
@@ -1,42 +1,35 @@
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
+// #include <zephyr/logging/log.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/adc.h>
-#include <nrfx_saadc.h>
 #include <zephyr/sys/printk.h>
 #include <stdio.h>
 #include <string.h>
+#include <nrfx_saadc.h>
+#include <nrfx_gpiote.h>
 
-//ADC
 
-#include <hal/nrf_saadc.h>
-#define ADC_DEVICE_NODE		DT_NODELABEL(adc)
-#define ADC_RESOLUTION 14
+#include <nrfx_example.h>
+#include <saadc_examples_common.h>
+
+#include <nrfx_log.h>
+
+#define ADC_1ST_CHANNEL_INPUT NRF_SAADC_INPUT_AIN1
+#define OUT_GPIO_PIN LOOPBACK_PIN_1B
+#define BUFFER_SIZE 2UL
+#define SAMPLING_ITERATIONS 4UL
+#define ADC_RESOLUTION NRF_SAADC_RESOLUTION_14BIT
+
 #define ADC_GAIN ADC_GAIN_1_4
 #define ADC_REFERENCE ADC_REF_VDD_1_4
 #define ADC_ACQUISITION_TIME ADC_ACQ_TIME(ADC_ACQ_TIME_MICROSECONDS, 20)
-#define ADC_1ST_CHANNEL_ID 1
-#define ADC_1ST_CHANNEL_INPUT NRF_SAADC_INPUT_AIN1
 #define SLEEP_TIME_MS 700
 #define ADC_OVERSAMPLING SAADC_OVERSAMPLE_OVERSAMPLE_Over256x
 
-const struct device *adc_dev = DEVICE_DT_GET(ADC_DEVICE_NODE);
+static nrf_saadc_value_t m_samples_buffer[BUFFER_SIZE];
 
 
-static const struct adc_channel_cfg m_1st_channel_cfg = {
-	.gain = ADC_GAIN,
-	.reference = ADC_REFERENCE,
-	.acquisition_time = ADC_ACQUISITION_TIME,
-	.channel_id = ADC_1ST_CHANNEL_ID,
-    .differential = 0,
-	
-#if defined(CONFIG_ADC_CONFIGURABLE_INPUTS)
-	.input_positive = ADC_1ST_CHANNEL_INPUT,
-#endif
-};
-
-/*
 nrfx_saadc_channel_t nrf_1st_channel = {
 	.channel_config = {
 		.resistor_p = NRF_SAADC_RESISTOR_DISABLED,
@@ -47,94 +40,92 @@ nrfx_saadc_channel_t nrf_1st_channel = {
 		.mode = NRF_SAADC_MODE_SINGLE_ENDED,
 		.burst = NRF_SAADC_BURST_ENABLED,
 	},
-	.pin_p = NRF_SAADC_INPUT_AIN1,
+	.pin_p = ADC_1ST_CHANNEL_INPUT,
 	.pin_n = NRF_SAADC_INPUT_DISABLED,
 	.channel_index = 0
 };
-*/
-nrfx_saadc_adv_config_t nrf_adv_1st_cfg = {
-	.oversampling = ADC_OVERSAMPLING,
-	.burst = NRF_SAADC_BURST_ENABLED,
-};
 
-#define BUFFER_SIZE 1
-static int16_t m_sample_buffer[BUFFER_SIZE];
-
-
-static int adc_sample(int volt)
-{
-	int ret;
-
-	const struct adc_sequence sequence = {
-		.channels = BIT(ADC_1ST_CHANNEL_ID),
-		.buffer = m_sample_buffer,
-		.buffer_size = sizeof(m_sample_buffer),
-		.resolution = ADC_RESOLUTION,
-		.oversampling = ADC_OVERSAMPLING,
-	};
-
-	if (!adc_dev) {
-		return -1;
-	}
-
-	ret = adc_read(adc_dev, &sequence);
-
-	/* Print the AIN0 values */
-
-	int32_t adc_voltage = m_sample_buffer[0];
-	printk("ADC raw value: %d\n", m_sample_buffer[0]);
-
-	ret = adc_raw_to_millivolts(adc_ref_internal(adc_dev), ADC_GAIN, ADC_RESOLUTION, &adc_voltage);
-	if (ret)
-	{
-		printk("raw_to_mili Broke!");
-		return ret;
-	}
-	
-	volt = (int) adc_voltage;
-	printk("Measured voltage: %d mV\n", volt);
-
-	return ret;
-}
 
 int main(void)
 {
 	struct gpio_dt_spec p17 = GPIO_DT_SPEC_GET(DT_NODELABEL(led0), gpios);
 	struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
 
-	if (!device_is_ready(p17.port)) {
-		return -1;
-	}
-	if (!device_is_ready(led1.port)) {
-		return -1;
-	}
+	if (!device_is_ready(p17.port)) return -1;
+	if (!device_is_ready(led1.port)) return -1;
+	
+	nrfx_err_t status;
+    (void)status;
+
+
+    printk("Starting the nrf9160 ADC-Terminal with the reworked adc configuration.\n");
+
+    status = nrfx_saadc_init(NRFX_SAADC_DEFAULT_CONFIG_IRQ_PRIORITY);
+    NRFX_ASSERT(status == NRFX_SUCCESS);
+
+    gpiote_pin_toggle_task_setup(OUT_GPIO_PIN);
+
+    status = nrfx_saadc_channel_config(&nrf_1st_channel);
+    NRFX_ASSERT(status == NRFX_SUCCESS);
+
+	nrfx_saadc_adv_config_t nrf_adv_1st_cfg = {
+		.oversampling = ADC_OVERSAMPLING,
+		.burst = NRF_SAADC_BURST_ENABLED,
+	};
+
+	uint32_t channel_mask = nrfx_saadc_channels_configured_get();
+    status = nrfx_saadc_advanced_mode_set(channel_mask,
+                                          ADC_RESOLUTION,
+                                          &nrf_adv_1st_cfg,
+                                          NULL);
+    NRFX_ASSERT(status == NRFX_SUCCESS);
+
+    status = nrfx_saadc_buffer_set(m_samples_buffer, BUFFER_SIZE);
+    NRFX_ASSERT(status == NRFX_SUCCESS);
+
 	int err;
 	gpio_pin_configure_dt(&p17, GPIO_OUTPUT);
 	gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
 
-	err = gpio_pin_set_dt(&p17, 1);
-	err = gpio_pin_set_dt(&led1, 1);
+
 	
-	if (err)
-	{
-		return err;
-	}
+	if (err) return err;
 
-	int volt = 0;
-
-	err = adc_channel_setup(adc_dev, &m_1st_channel_cfg);
-	if (err) {
-		printk("Error in adc setup: %d\n", err);
-		return err;
-	}
-
+    uint16_t sampling_index = 0;
 	while (1) {
-		err = adc_sample(volt);
-		if (err) {
-			printk("Error in adc sampling: %d\n", err);
-		}
-		k_msleep(SLEEP_TIME_MS);
+        	err = gpio_pin_set_dt(&p17, 1);
+	        err = gpio_pin_set_dt(&led1, 1);
+            nrfx_gpiote_out_task_trigger(OUT_GPIO_PIN);
+
+            status = nrfx_saadc_offset_calibrate(NULL);
+            NRFX_ASSERT(status == NRFX_SUCCESS);
+            printk("Calibration in the blocking manner finished successfully.\n");
+
+            /*
+             * If function nrfx_saadc_mode_trigger() returns NRFX_ERROR_BUSY it means that the conversion in the advanced
+             * blocking mode is still being performed. Then the function must be called again to continue the conversion.
+             */
+            while ((status = nrfx_saadc_mode_trigger()) == NRFX_ERROR_BUSY)
+            {}
+            NRFX_ASSERT(status == NRFX_SUCCESS);
+            printk("Sample %d\n", sampling_index + 1);
+
+            for (uint32_t buffer_index = 0; buffer_index < BUFFER_SIZE; buffer_index++)
+            {
+                printk("[Sample %u] value == %d\n", buffer_index,
+                                                         m_samples_buffer[buffer_index]);
+            }
+
+            status = nrfx_saadc_buffer_set(m_samples_buffer, BUFFER_SIZE);
+            NRFX_ASSERT(status == NRFX_SUCCESS);
+
+            gpio_pin_set_dt(&p17, 0);
+		    gpio_pin_set_dt(&led1, 0);
+
+            sampling_index++;
+
+            k_msleep(SLEEP_TIME_MS);
+
 	}
-		gpio_pin_set_dt(&p17, 0);
-		gpio_pin_set_dt(&led1, 0);
+
 }
